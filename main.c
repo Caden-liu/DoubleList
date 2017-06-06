9dbd89ce002d599e001140de23418ce5c79a25c1
liuxj <476611059@qq.com>
2017-06-06 11:48:40 +0800

    The second change

--- main.c
+++ main.c
@@ -20,6 +20,6 @@ int main(void)
     num =  count_number_in_double_link(GClist);
     printf("list cnt:%d\n",num);
     print_double_link_node(GClist);
-
+    printf("Version:0.01\n");
     return 0;
 }
